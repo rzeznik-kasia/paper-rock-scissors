d3bd35d0614da033e191a30a4d9c4dbe69132e97
rzeznik-kasia <rzeznik.kasia@gmail.com>
2016-07-19 12:59:22 +0200

    logika rund

--- PaperRockScissors/src/prsGame/PRSGame.java
+++ PaperRockScissors/src/prsGame/PRSGame.java
@@ -4,6 +4,7 @@ package prsGame;
 public class PRSGame {
 
 	public static void main(String[] args) {
+		int gameCount = 3;
 		GestureFactory.gestureInit();
 		String message = "Pierwszy gracz podaje imię: ";
 		String name = ScanerFacade.handlNameInput(message);
@@ -13,14 +14,32 @@ public class PRSGame {
 		name = ScanerFacade.handlNameInput(message);
 		Player p2 = new Player(name);
 		
-		message = p1.getName() + " podaje znak jakim będzie grał ("
-				+ GestureFactory.getAvailableGestures() + "): ";
 		
-		Thing user1Thing = p1.getGesture();
-		Thing user2Thing = p2.getGesture();
+		while (gameCount > 0) {
+			message = p1.getName() + " podaje znak jakim będzie grał ("
+					+ GestureFactory.getAvailableGestures() + "): ";
+			
+			Thing user1Thing = p1.getGesture();
+			Thing user2Thing = p2.getGesture();
+			
+			
+			if (user1Thing.isStrongerThan(user2Thing)) {
+				System.out.println("Koniec gry. Wygral gracz 1");
+				return;
+			}
+			else if (user2Thing.isStrongerThan(user1Thing)) {
+				System.out.println("Koniec gry. Wygral gracz 2");
+				return;
+			}
+			else {
+				System.out.println("remis");
+				gameCount--;
+				if (gameCount == 0) {
+					System.out.println("Koniec gry. Remis");
+				}
+			}
 		
-		Boolean b = user1Thing.isStrongerThan(user2Thing);
-		System.out.println("koniec");
+		}	
 	}
 
 }
